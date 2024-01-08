c2f9f1c8372d0f6c1889e5ad7b96651a6fadc3ba
jxhnkndl <jkroyston@gmail.com>
2024-01-08 14:33:16 -0500

    create user model

---
+++ server/models/User.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [8, 'Password must be at least 8 characters long'],
+    max: [32, 'Password must not exceed 32 characters'],
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  bio: {
+    type: String,
+    trim: true,
+  },
+  roles: [
+    {
+      type: String,
+    },
+  ],
+  artists: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Artist',
+    },
+  ],
+});
+
+// Hash password for new accounts and accounts updating password field
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// Check credentials when user logs in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
