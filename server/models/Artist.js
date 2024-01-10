dd1fd2a2c7132167163cbb33b4d4e12a8aa49930
jxhnkndl <jkroyston@gmail.com>
2024-01-10 10:32:34 -0500

    reconfigure database structure to convert artist into a type of user document

--- server/models/Artist.js
+++ server/models/Artist.js
@@ -1,10 +1,50 @@
 const { Schema, model } = require('mongoose');
 
 const artistSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [8, 'Password must be at least 8 characters long'],
+    max: [32, 'Password must not exceed 32 characters'],
+  },
   name: {
     type: String,
     reqired: true,
     trim: true,
+  },
+  bio: {
+    type: String,
+    trim: true,
+  },
+  profilePhoto: {
+    type: String
+  },
+  label: {
+    name: { type: String },
+    phone: { type: String },
+    email: { type: String }
+  },
+  manager: {
+    name: { type: String },
+    phone: { type: String },
+    email: { type: String }
+  },
+  bookingAgent: {
+    name: { type: String },
+    phone: { type: String },
+    email: { type: String }
+  },
+  tourManager: {
+    name: { type: String },
+    phone: { type: String },
+    email: { type: String }
   },
   concerts: [
     {
