455e83e71a2962bb50872fd8eba552843fd1e8fa
jxhnkndl <jkroyston@gmail.com>
2024-01-10 10:32:52 -0500

    refactor seeds to fit new database schema

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -16,46 +16,31 @@ const seedDb = async () => {
       await Artist.deleteMany();
       await Concert.deleteMany();
 
+      // Seed artist
+      console.log('CREATING ARTIST 👀');
+
+      // Create new artist and make first user in database the artist admin
+      const artist = await Artist.create({ ...artistSeeds[0] });
+
+      const userIds = [];
+
+      // Seed Users
       console.log('CREATING USERS 👀');
 
-      // Save user and artist ids to associate with other documents
-      const userIds = [];
-      const artistIds = [];
-
-      // Seed users
       for (let i = 0; i < userSeeds.length; i++) {
         console.log(`CREATING USER #${i} ✅`);
 
-        const user = await User.create(userSeeds[i]);
-
-        // Save the user id to associate with an artist
-        userIds.push(user._id);
-      }
-
-      console.log('CREATING ARTISTS 👀');
-
-      // Seed artists
-      for (let i = 0; i < artistSeeds.length; i++) {
-        console.log(`CREATING ARTIST #${i} ✅`);
-
-        // Create new artist and make first user in database the artist admin
-        const artist = await Artist.create({
-          ...artistSeeds[i],
-          admin: userIds[0],
-          crew: [userIds[0]]
+        const user = await User.create({
+          ...userSeeds[i],
+          artists: [artist._id]
         });
 
-        // Update the admin user's profile to include the artist id
-        await User.findOneAndUpdate(
-          { _id: userIds[0] },
-          { $addToSet: { artists: artist._id } },
-          { new: true }
-        );
+        artist.crew.push(user._id);
 
-        // Save the artist id to associate with concerts
-        artistIds.push(artist._id);
+        await artist.save();
       }
 
+      // Seed concerts
       console.log('CREATING CONCERTS 👀');
 
       for (let i = 0; i < concertSeeds.length; i++) {
@@ -64,12 +49,9 @@ const seedDb = async () => {
         // Create concert
         const concert = await Concert.create(concertSeeds[i]);
 
-        // Associate concert with artist
-        await Artist.findOneAndUpdate(
-          { _id: artistIds[0] },
-          { $addToSet: { concerts: concert._id } },
-          { new: true }
-        );
+        artist.concerts.push(concert._id);
+
+        await artist.save();
       }
     } catch (error) {
       console.log(error);
